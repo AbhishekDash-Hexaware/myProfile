0214822e1c1114cfd8972a890ada26e1604cf7a5
AbhishekDa <abhishekda@hexaware.com>
2017-12-14 21:13:21 +0530

    face change

--- index.js
+++ index.js
@@ -1,12 +1,17 @@
 
 const express = require('express')
 const app = express()
+var morgan = require('morgan')
 
-app.use(express.static(__dirname));
+app.use(express.static(__dirname+"/content"));
+app.use(morgan('common'))
+
 var port = process.env.PORT || 5000;
 
-app.get('/home', (req, res) => res.send('Hello world'))
+app.get('/wala', (req, res) =>{ 
+res.send('Hello world')
+});
 
-app.get('/',(req,res)=> res.sendFile(__dirname+"/index.html"))
+app.get('/',(req,res)=> res.sendFile(__dirname+"/content/index.html"))
 
-app.listen(port, () => console.log('Example app listening on port 3000!'))
+app.listen(port, ()=>console.log('Example app listening on port 3000!'))
