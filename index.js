2c42dcd8deccc1812e1c7dcbe010890c724a1e99
AbhishekDa <abhishekda@hexaware.com>
2017-12-17 03:51:10 +0530

    removed image rec

--- index.js
+++ index.js
@@ -1,10 +1,9 @@
-const Clarifai = require('clarifai');
 const express = require('express')
 var morgan = require('morgan')
-var imagerec =require('./apps/app') 
+ 
 
 const app = express()
-const Clariapp = new Clarifai.App({ apiKey: 'ac168bb5e1fa49e0a7374b68c88603da' });
+
 
 
 app.use(express.static(__dirname+"/content"));
@@ -20,7 +19,7 @@ app.get('/',(req,res)=>
 res.sendFile(__dirname+"/content/index.html"));
 
 app.get('/imagerec',(req,res)=>{
-    recognise(req,res);
+    res.send("development inprogress")
 })
 
 app.listen(port, (req,res)=>
